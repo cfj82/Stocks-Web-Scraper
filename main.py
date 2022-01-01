cd113c202dc490680ea535cfec4c7c1b6fff522c
Colin Fowler <92275816+cfj82@users.noreply.github.com>
2021-12-31 16:33:15 -0800

    added email function

--- main.py
+++ main.py
@@ -3,19 +3,10 @@
 from bs4 import BeautifulSoup
 from datetime import datetime
 import requests
-
-"""
-url = requests.get('https://www.fool.com/investing/top-stocks-to-buy/')
-soup =bs4.BeautifulSoup(url.content, 'html.parser')
-stocks = soup.find(class_='related-tickers')
-stock_picks = stocks.find_all(class_='ticker-text-wrap')
-# get stock names
-stock_names = []
-for stock in stock_picks:
-    stock_names.append(stock.h3.get_text())
-    print(stock_names)
-"""
-# todo add email bot
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import socket
 
 def scrape_stock():
     search_stock = input("What is the stock ticker you would like to search?\n").upper()  # must convert to uppercase
@@ -36,7 +27,47 @@ def scrape_stock():
         stock_price = soup.find('fin-streamer', {'class':'Fw(b) Fz(36px) Mb(-4px) D(ib)'}).text
         t = datetime.now()
         time_scraped = t.strftime("%d/%m/%Y %H:%M")
-        print(stock_name + "'s Price is: $" + stock_price + " at " + time_scraped)
+        result = stock_name + "\'s Price is: $" + stock_price + " at " + time_scraped
+        print(result)
+        # call email function
+        email_bot(stock_name, result)
 
 
+def email_bot(stock_name, result):
+    from_address = 'cfowlerNoReply@gmail.com'
+    from_pw = 'JhgJhg>.'
+    to_address = 'colinjunfowler@gmail.com'
+    mail_content = """result"""
+
+    # set up MMIME
+    message = MIMEMultipart()
+    message['From'] = from_address
+    message['To'] = to_address
+    message['Subject'] = "{stock_name} + 'Stock Price'"
+    # body and attachments for the email
+    message.attach(MIMEText(mail_content, 'plain'))
+
+    try:
+        # create SMTP session for sending email
+        server = smtplib.SMTP('64.233.184.108')  # IP address of smtp.gmail.com to bypass DNS resolution
+        server.starttls()  # enables security
+        server.login(from_address, from_pw)
+        text = message.as_string()
+        server.sendmail(from_address, to_address, text)
+        server.quit()
+        print("Email Sent Successfully!")
+    except Exception as ex:
+        print("Something went wrong" + str(ex))
+
+'''    try:
+        # send email
+        server = smtplib.SMTP_SSL("smtp.gmail.com", 465)  # stating from gmail and port 465
+        server.login(sender_address, pw)
+        server.sendmail(sender_address, email_recepients, mail_message)  # (email from, email to, email message)
+        server.close()
+        print("Email Sent Successfully!")
+    except Exception as ex:
+        print("Something went wrong" + str(ex))'''
+
+# run
 scrape_stock()
